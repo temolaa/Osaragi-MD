399eed3c401c21fa132fbb3eca8760e75b120616
temolaa <teleakun1509@gmail.com>
2024-12-09 11:45:18 +0800

    Update adiwConfig.js

--- all/security/adiwConfig.js
+++ all/security/adiwConfig.js
@@ -1,11 +1,11 @@
 module.exports = {
   'approval': {
-    'num': '6289508082845',
+    'num': '6283872957553',
     'text': "oke",
     'set': "👀 Harus mendapatkan persetujuan oleh creator script, Jika ingin memakai script ini",
     'greet': "*Disetujui Oleh Creator, Silahkan Restart Panel Atau Run Ulang script* ☠️ "
   },
-  'creatorScript': '6289508082845',
+  'creatorScript': '6283872957553',
   'filePath': "./all/approval",
   'checkFilePath': "../setting/osaragi.js",
   'codeToDetect': 'main();'
